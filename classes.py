59d87018e67563d26a25bf3a0d7832a976fcf594
Maksmvs <142376726+Maksmvs@users.noreply.github.com>
2023-11-01 16:04:16 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -148,3 +148,45 @@ class AddressBook(UserDict):
     # удаление записи в адресной книге
     def del_record(self, args):
         self.data.pop(int(args[0]))
+
+class NotesManager:
+    def __init__(self):
+        # Ініціалізація менеджера нотаток і отримання доступу до даних помічника
+        self.data = assistant.data
+
+    def add_note(self, text):
+        # Додавання нової нотатки до списку нотаток
+        note = {'text': text, 'tags': []}
+        self.data['notes'].append(note)
+        assistant.save_data()
+
+    def find_notes_by_tag(self, tag):
+        # Пошук нотаток за вказаним тегом
+        results = [note for note in self.data['notes'] if tag in note['tags']]
+        return results
+
+    def show_all_notes(self):
+        # Виведення всіх існуючих нотаток
+        if self.data['notes']:
+            for idx, note in enumerate(self.data['notes']):
+                print(f"{idx + 1}. {note['text']}")
+        else:
+            print("Нотаток немає.")
+
+    def edit_note(self, note_idx, new_text):
+        # Редагування вибраної нотатки
+        if 0 <= note_idx < len(self.data['notes']):
+            self.data['notes'][note_idx]['text'] = new_text
+            assistant.save_data()
+            print("Нотатку відредаговано.")
+        else:
+            print("Неправильний номер нотатки.")
+
+    def delete_note(self, note_idx):
+        # Видалення вибраної нотатки
+        if 0 <= note_idx < len(self.data['notes']):
+            del self.data['notes'][note_idx]
+            assistant.save_data()
+            print("Нотатку видалено.")
+        else:
+            print("Неправильний номер нотатки.")
