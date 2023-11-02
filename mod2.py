903185f0ceeaadef198e4a5da8894800592ab577
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2023-11-02 22:41:09 +0200

    add upcoming_birthdays func and tab autocomplete

--- mod2.py
+++ mod2.py
@@ -1,12 +1,26 @@
 from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import WordCompleter
+import datetime
 
-# Внешний пакет нужно сначала установить в терминале: pip install prompt_toolkit
+# You need to install the external package in the terminal first: pip install prompt_toolkit
 
 commands = WordCompleter([
     "close", "exit", "hello", "add", "edit", "del", 
     "add-phone", "edit-phone", "del-phone", "all", 
     "add-email", "edit-email", "del-email", 
-    "birthday", "del-birthday", "note", "help"], ignore_case=True)
+    "birthday", "del-birthday", "next-birthdays", "note", "help"], ignore_case=True)
 session = PromptSession(completer=commands)
 nsession = PromptSession()
+
+def upcoming_birthdays(book, args):
+    today = datetime.date.today()
+    birthday_contacts = []
+    this_year = today.year
+
+    for _, record in book.data.items():
+        if record.birthday is not None:
+            days_until_birthday = (record.birthday.value.replace(year=this_year) - today).days
+            if 0 <= days_until_birthday <= int(args[0]):
+                birthday_contacts.append(record)
+
+    return birthday_contacts
