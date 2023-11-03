fcb1a06070ba5e62de8ccafb6592ddd1a7748404
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2023-11-03 11:49:05 +0200

    add doc comment and refactor help command

--- mod4.py
+++ mod4.py
@@ -95,24 +95,31 @@ def find_in_records(book:AddressBook, args:list):
 
 # help
 def show_help():
-    help_string =   'Command [Parameters] -> Description\n\n'
-    help_string +=  'add [Name] -> create new user [Name] in adress book\n'
-    help_string +=  'edit [Contact_id] [new_Name] -> edit name of [Contact_id] to [new_Name]\n'
-    help_string +=  'del [Contact_id] -> remove user [Contact_id] from adress book\n'
-    help_string +=  'add-phone [Contact_id] [Phone] -> add to user [Contact_id] a [Phone]\n'
-    help_string +=  'edit-phone [Contact_id] [Phone] [new_Phone] -> replace for user [Contact_id] a [Phone] by [new_Phone]\n'
-    help_string +=  'del-phone [Name] [Phone] -> remove phone [Phone] from user [Name]\n'
-    help_string +=  'add-email [Contact_id] [Email] -> add to user [Contact_id] an [Email]\n'
-    help_string +=  'edit-email [Contact_id] [Email] [new_Email] -> replace for user [Contact_id] an [Email] by [new_Email]\n'
-    help_string +=  'del-email [Contact_id] [Email] -> remove email [Email] from user [Contact_id]\n'
-    help_string +=  'address Contact_id] [Address] -> set for user [Name] an address [Address]\n'
-    help_string +=  'del-address [Contact_id] -> remove address from [Contact_id]\n'
-    help_string +=  'birthday [Contact_id] [Birthday] -> set for user [Contact_id] a birthday at [Birthday]\n'
-    help_string +=  'del-birthday [Contact_id] -> remove birthday from [Contact_id]\n'
-    help_string +=  'all -> list all information about users\n'
-    help_string +=  'help -> List all bot commands\n'
-    help_string +=  'find [serchstring] -> list all users with [serchstring] data in Name, Phones, Address, Emails, Birthdays. [serchstring] must be 2 symbols minimum\n'
-    help_string +=  'close -> exit the bot\n'
-    help_string +=  'exit -> exit the bot\n'
-    help_string +=  'note -> switch to note functional\n'
+    commands = [['Command', 'Parameters', 'Description'],
+                   ['all', '', 'list all information about users'],
+                   ['add', '[Name]', 'create new user [Name] in adress book'], 
+                   ['edit', '[Contact_id] [new_Name]', 'edit name of [Contact_id] to [new_Name]'],
+                   ['del', '[Contact_id]', 'remove user [Contact_id] from adress book'],
+                   ['add-phone', '[Contact_id] [Phone]', 'add to user [Contact_id] a [Phone]'],
+                   ['edit-phone', '[Contact_id] [Phone] [new_Phone]', 'replace for user [Contact_id] a [Phone] by [new_Phone]'],
+                   ['del-phone', '[Name] [Phone]', 'remove phone [Phone] from user [Name]'],
+                   ['add-email', '[Contact_id] [Email]', 'add to user [Contact_id] an [Email]'],
+                   ['edit-email', '[Contact_id] [Email] [new_Email]', 'replace for user [Contact_id] an [Email] by [new_Email]'],
+                   ['del-email', '[Contact_id] [Email]', 'remove email [Email] from user [Contact_id]'],
+                   ['address', '[Contact_id] [Address]', 'set for user [Name] an address [Address]'],
+                   ['del-address', '[Contact_id]', 'remove address from [Contact_id]'],
+                   ['birthday', '[Contact_id] [Birthday]', 'set for user [Contact_id] a birthday at [Birthday]'],
+                   ['del-birthday', '[Contact_id]', 'remove birthday from [Contact_id]'],
+                   ['find', '[serchstring]', 'list all users with [serchstring] data in Name, Phones, Address, Emails, Birthdays. [serchstring] must be 2 symbols minimum'],
+                   ['next-birthdays', '[int]', 'shows upcoming birthdays if exist in period from today till [int] days'],
+                   ['close, exit', '', 'exit the bot'],
+                   ['note', '', 'switch to note functional'],
+                   ['help', '', 'list all bot commands']]
+    dashes = "{0:<14} + {1:<32} + {2:^12} \n".format("-" * 14, "-" * 32, "-" * 12)
+    help_string = ''
+
+    for i in commands:
+        help_string += f'{i[0]:^14} | {i[1]:^32} | {i[2]:^12} \n'
+        help_string += dashes
+    
     print(help_string)
