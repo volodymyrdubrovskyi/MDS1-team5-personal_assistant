15e2916f650672f642fd1f3eb0abb5df07c3b5b5
volodymyrdubrovskyi <vladimir.dubrovskiy@gmail.com>
2023-11-04 18:19:27 +0200

    refactoring

---
+++ data/global_declarations.py
@@ -0,0 +1,67 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from data.classes import AddressBook, NoteBook
+
+# You need to install the external package in the terminal first: pip install prompt_toolkit
+commands = WordCompleter([
+    "close", "exit", "hello", "add", "edit", "del", "address", "del-address",
+    "add-phone", "edit-phone", "del-phone", "all", 
+    "add-email", "edit-email", "del-email", 
+    "birthday", "del-birthday", "next-birthdays", "note", "help", "sort-tag",
+    "edit-note", "all-notes", "del-note", "add-tag", "del-tag", "find-notes, find-tag"], ignore_case=True)
+session = PromptSession(completer=commands)
+
+def exit_procedure(book:AddressBook, nbook:NoteBook):
+    """
+    Saving AddressBook to file
+    Args:
+        book (AddressBook): An AddressBook instance containing contact information.
+    """
+    book.save_to_file()
+    nbook.save_to_file()
+    print('Good bye!')
+
+# input commands from terminal
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+# help
+def show_help():
+    commands = [['Command', 'Parameters', 'Description'],
+                   ['all', '', 'list all information about users'],
+                   ['add', '[Name]', 'create new user [Name] in adress book'], 
+                   ['edit', '[Contact_id] [new_Name]', 'edit name of [Contact_id] to [new_Name]'],
+                   ['del', '[Contact_id]', 'remove user [Contact_id] from adress book'],
+                   ['add-phone', '[Contact_id] [Phone]', 'add to user [Contact_id] a [Phone]'],
+                   ['edit-phone', '[Contact_id] [Phone] [new_Phone]', 'replace for user [Contact_id] a [Phone] by [new_Phone]'],
+                   ['del-phone', '[Name] [Phone]', 'remove phone [Phone] from user [Name]'],
+                   ['add-email', '[Contact_id] [Email]', 'add to user [Contact_id] an [Email]'],
+                   ['edit-email', '[Contact_id] [Email] [new_Email]', 'replace for user [Contact_id] an [Email] by [new_Email]'],
+                   ['del-email', '[Contact_id] [Email]', 'remove email [Email] from user [Contact_id]'],
+                   ['address', '[Contact_id] [Address]', 'set for user [Name] an address [Address]'],
+                   ['del-address', '[Contact_id]', 'remove address from [Contact_id]'],
+                   ['birthday', '[Contact_id] [Birthday]', 'set for user [Contact_id] a birthday at [Birthday]'],
+                   ['del-birthday', '[Contact_id]', 'remove birthday from [Contact_id]'],
+                   ['find', '[serchstring]', 'list all users with [serchstring] data in Name, Phones, Address, Emails, Birthdays. [serchstring] must be 2 symbols minimum'],
+                   ['next-birthdays', '[int]', 'shows upcoming birthdays if exist in period from today till [int] days'],
+                   ['note', '', 'Add a note to Note Book'],
+                   ['all-notes', '', 'list all notes'],
+                   ['edit-note', '[Note_id] [Note]', 'edit text of [Note_id] Note'],
+                   ['del-note', '[Note_id]', 'Remove [Note_id] note from Note Book'],
+                   ['add-tag', '[Note_id] [Tag]', 'add [Tag] to note [Note_id]'],
+                   ['del-tag', '[Note_id] [Tag]', 'remove [Tag] from note [Note_id]'],
+                   ['find-notes', '[serchstring]', 'list all Notes with [serchstring] data in note and tags.[serchstring] must be 2 symbols minimum'],
+                   ['find-tags', '[serchstring]', 'list all Notes with [serchstring] data in tags.[serchstring] must be 2 symbols minimum'],
+                   ['sort-tag', '', 'list all Notes sorted by number of tags'],
+                   ['close, exit', '', 'exit the bot'],
+                   ['help', '', 'list all bot commands']]
+    dashes = "{0:<14} + {1:<32} + {2:^12} \n".format("-" * 14, "-" * 32, "-" * 12)
+    help_string = ''
+
+    for i in commands:
+        help_string += f'{i[0]:^14} | {i[1]:^32} | {i[2]:^12} \n'
+        #help_string += dashes
+    
+    print(help_string)
--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-from classes import *
-from mod4 import *
-from mod2 import *
-from mod3 import *
-from mod1 import *
-from prompt_toolkit.history import InMemoryHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from data.global_declarations import commands, session, exit_procedure, parse_input, show_help
+from data.classes import AddressBook, NoteBook
 
+from data.address_book_functions import (   upcoming_birthdays, add_email_in_rec, edit_email_in_rec, del_email_in_rec, 
+                                            birthday_record, del_birthday, address_record, del_address, 
+                                            add_record, edit_record, del_record, add_phone_in_rec, edit_phone_in_rec, del_phone_in_rec,
+                                            find_in_records  )
 
-# считываем с терминала команду
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
+from data.notebook_functions import find_in_notes, find_in_tags, sort_by_tags, add_note, edit_note, del_note, add_tag, del_tag
+
 
 def main():
 
     print('Welcome to the assistant bot!')
+    print('Type "help" to see all commands list and descriptions')
 
     # AddressBook load from 'abook.dat' file. If file doesn't exist, create new empty AddressBook
     try:
@@ -23,6 +21,11 @@ def main():
     except:
         book = AddressBook()
 
+    # NoteBook load from 'nbook.dat' file. If file doesn't exist, create new empty NoteBook
+    try:
+        nbook = NoteBook().read_from_file()
+    except:
+        nbook = NoteBook()
     
     while True:
         user_input = session.prompt("Enter a command: ", auto_suggest=AutoSuggestFromHistory(), complete_while_typing=False)
@@ -30,7 +33,7 @@ def main():
             command, *args = parse_input(user_input)
             
             if command in ['close', 'exit']:
-                exit_procedure(book)
+                exit_procedure(book, nbook)
                 break
 
             elif command == 'hello':
@@ -78,11 +81,36 @@ def main():
             elif command == 'find':
                 find_in_records(book, args)
 
+            elif command == 'find-notes':
+                find_in_notes(nbook, args)
+
+            elif command == 'find-tag':
+                find_in_tags(nbook, args)
+
+            elif command == 'sort-tag':
+                sort_by_tags(nbook, args)
+
             elif command == 'help':
                 show_help()
 
             elif command == 'note':
-                notes_func()
+                add_note(nbook, args)
+
+            elif command == 'edit-note':
+                edit_note(nbook, args)
+
+            elif command == 'del-note':
+                del_note(nbook, args)
+
+            elif command == 'add-tag':
+                add_tag(nbook, args)
+
+            elif command == 'del-tag':
+                del_tag(nbook, args)
+
+            elif command == 'all-notes':
+                for _, note in nbook.data.items():
+                    print(note)
 
             elif command == 'next-birthdays':
                 birthdays = upcoming_birthdays(book, args)
